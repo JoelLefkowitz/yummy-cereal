80b4fb3bdaf20d8cdb078252e334dc7a03c6d753
Joel Lefkowitz <joellefkowitz@hotmail.com>
2020-07-05 14:21:31 +0100

    Bump version: 2.0.1 → 2.0.2

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@ path_guard("../..")
 
 
 project = "Yummy Cereal"
-version = "2.0.1"
+version = "2.0.2"
 master_doc = "index"
 author = "Joel Lefkowitz"
 copyright = f"{datetime.datetime.now().year}, {author}"
